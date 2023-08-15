406c2e475dae2f5b13cfef002979446b8086cc64
danijelTxFusion <danijel@txfusion.com>
2023-08-15 14:33:21 +0200

    chore: add script

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+## 0.3.1 (2023-08-15)
+
+#### Bug Fixes
+
+* release pipeline (b0984c6d)
+* add ci (e31fe07d)
+
---
+++ ci.sh
@@ -0,0 +1,1 @@
+npx go-semantic-release --no-ci --provider git --allow-initial-development-versions --changelog CHANGELOG.md
