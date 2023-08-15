e0231c454ff4c7c0e8763bf1fe50cebd86b2efd3
danijelTxFusion <danijel@txfusion.com>
2023-08-15 22:59:07 +0200

    fix: add ci script

--- ci.sh
+++ ci.sh
@@ -1,1 +1,15 @@
-npx go-semantic-release --no-ci --provider git --allow-initial-development-versions --changelog CHANGELOG.md
+#!/bin/bash
+
+npx go-semantic-release \
+  --dry \
+  --no-ci \
+  --provider git \
+  --allow-initial-development-versions \
+  --changelog tmp.md
+
+if [ -e CHANGELOG.md ]; then
+    cat CHANGELOG.md >> tmp.md
+    mv -f tmp.md CHANGELOG.md
+else
+    mv tmp.md CHANGELOG.md
+fi
