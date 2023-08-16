abe1ad2d97572c8c0cbc210efbe0f7ab84292736
danijelTxFusion <danijel@txfusion.com>
2023-08-16 14:24:57 +0200

    chore: update commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,10 +3,4 @@ module.exports =
         "extends": [
             "@commitlint/config-conventional"
         ],
-        "rules": {
-            "type-enum": [2, "always", ["feat", "fix", "chore"]],
-            "footer-max-line-length": [0, "always"],
-            "header-max-length": [2, "always", 120],
-            "subject-case": [0]
-        }
     }
