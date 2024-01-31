2a143dcb135a78bfa7451fc3ea9845be54bbd7c3
danijelTxFusion <danijel@txfusion.io>
2024-02-01 00:33:52 +0100

    build: fix the contracts name when genereting them

--- scripts/generate-contracts/entrypoint.sh
+++ scripts/generate-contracts/entrypoint.sh
@@ -4,49 +4,57 @@ mkdir -p /contracts/contractdeployer
 abigen  \
 	--abi /abi/IContractDeployer.json \
 	--out /contracts/contractdeployer/contract_deployer.go \
-	--pkg contractdeployer
+	--pkg contractdeployer \
+	--type IContractDeployer
 
 mkdir -p /contracts/ethtoken
 abigen  \
 	--abi /abi/IEthToken.json \
 	--out /contracts/ethtoken/eth_token.go \
-	--pkg ethtoken
+	--pkg ethtoken \
+	--type IEthToken
 
 mkdir -p /contracts/l1bridge
 abigen  \
 	--abi /abi/IL1Bridge.json \
 	--out /contracts/l1bridge/l1_bridge.go \
-	--pkg l1bridge
+	--pkg l1bridge \
+	--type IL1Bridge
 
 mkdir -p /contracts/l1messenger
 abigen  \
 	--abi /abi/IL1Messenger.json \
 	--out /contracts/l1messenger/l1_messenger.go \
-	--pkg l1messenger
+	--pkg l1messenger \
+	--type IL1Messenger
 
 mkdir -p /contracts/l2bridge
 abigen  \
 	--abi /abi/IL2Bridge.json \
 	--out /contracts/l2bridge/l2_bridge.go \
-	--pkg l2bridge
+	--pkg l2bridge \
+	--type IL2Bridge
 
 mkdir -p /contracts/nonceholder
 abigen  \
 	--abi /abi/INonceHolder.json \
 	--out /contracts/nonceholder/nonce_holder.go \
-	--pkg nonceholder
+	--pkg nonceholder \
+	--type INonceHolder
 
 mkdir -p /contracts/paymasterflow
 abigen  \
 	--abi /abi/IPaymasterFlow.json \
 	--out /contracts/paymasterflow/paymaster_flow.go \
-	--pkg paymasterflow
+	--pkg paymasterflow \
+	--type IPaymasterFlow
 
 mkdir -p /contracts/zksync
 abigen  \
 	--abi /abi/IZkSync.json \
 	--out /contracts/zksync/zk_sync.go \
-	--pkg zksync
+	--pkg zksync \
+	--type IZkSync
 
 echo "Folder content"
 ls -alhR /contracts
