77cd3859360ad7d59afc5126abb7f5f16342c6fa
danijelTxFusion <danijel@txfusion.io>
2024-02-21 16:43:07 +0100

    test: increase code coverage

---
+++ test/account_abstraction_test.go
@@ -0,0 +1,147 @@
+package test
+
+import (
+	"context"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/assert"
+	"github.com/zksync-sdk/zksync2-go/accounts"
+	"github.com/zksync-sdk/zksync2-go/clients"
+	"github.com/zksync-sdk/zksync2-go/types"
+	"github.com/zksync-sdk/zksync2-go/utils"
+	"math/big"
+	"testing"
+)
+
+func TestIntegration_ApprovalPaymaster(t *testing.T) {
+	AirdropAmount := big.NewInt(10)
+	MinimalAllowance := big.NewInt(1)
+	MintAmount := big.NewInt(7)
+
+	client, err := clients.Dial(ZkSyncEraProvider)
+	defer client.Close()
+	assert.NoError(t, err, "clients.Dial should not return an error")
+
+	wallet, err := accounts.NewWallet(common.Hex2Bytes(PrivateKey), &client, nil)
+	assert.NoError(t, err, "NewWallet should not return an error")
+
+	// ====== Deploy Token ======
+	tokenAbi, err := TokenMetaData.GetAbi()
+	assert.NoError(t, err, "GetAbi should not return an error")
+
+	tokenConstructor, err := tokenAbi.Pack("", "Crown", "Crown", uint8(18))
+	assert.NoError(t, err, "Pack should not return an error")
+
+	tokenDeployHash, err := wallet.DeployWithCreate(nil, accounts.CreateTransaction{
+		Bytecode: common.FromHex(TokenMetaData.Bin),
+		Calldata: tokenConstructor,
+	})
+	assert.NoError(t, err, "Deploy should not return an error")
+
+	tokenDeployReceipt, err := client.WaitMined(context.Background(), tokenDeployHash)
+	assert.NoError(t, err, "client.WaitMined should not return an error")
+
+	tokenAddress := tokenDeployReceipt.ContractAddress
+	assert.NotNil(t, tokenAddress, "Contract should be deployed")
+
+	// ===== Mint tokens to wallet =====
+	token, err := NewToken(tokenAddress, client)
+	assert.NoError(t, err, "NewToken should not return an error")
+
+	opts, err := bind.NewKeyedTransactorWithChainID(wallet.Signer().PrivateKey(), wallet.Signer().Domain().ChainId)
+	assert.NoError(t, err, "NewKeyedTransactorWithChainID should not return an error")
+
+	mint, err := token.Mint(opts, wallet.Address(), AirdropAmount)
+	assert.NoError(t, err, "Mint should not return an error")
+
+	_, err = client.WaitMined(context.Background(), mint.Hash())
+	assert.NoError(t, err, "client.WaitMined should not return an error")
+
+	// ===== Deploy Paymaster =====
+	_, paymasterAbi, bytecode, err := utils.ReadStandardJson("./testfiles/Paymaster.json")
+	assert.NoError(t, err, "ReadStandardJson should not return an error")
+
+	paymasterConstructor, err := paymasterAbi.Pack("", tokenAddress)
+	assert.NoError(t, err, "Pack should not return an error")
+
+	paymasterDeployHash, err := wallet.Deploy(nil, accounts.Create2Transaction{
+		Bytecode: bytecode,
+		Calldata: paymasterConstructor,
+	})
+	assert.NoError(t, err, "DeployWithCreate should not return an error")
+
+	paymasterDeployReceipt, err := client.WaitMined(context.Background(), paymasterDeployHash)
+	assert.NoError(t, err, "client.WaitMined should not return an error")
+
+	paymasterAddress := paymasterDeployReceipt.ContractAddress
+	assert.NotNil(t, paymasterAddress, "Contract should be deployed")
+
+	// ===== Transfer some ETH to paymaster, so it can pay fee with ETH =====
+	transferTx, err := wallet.Transfer(nil, accounts.TransferTransaction{
+		To:     paymasterAddress,
+		Amount: big.NewInt(2_000_000_000_000_000_000),
+		Token:  utils.EthAddress,
+	})
+	assert.NoError(t, err, "Transfer should not return an error")
+
+	_, err = client.WaitMined(context.Background(), transferTx.Hash())
+	assert.NoError(t, err, "client.WaitMined should not return an error")
+
+	// Read token and ETH balances from user and paymaster accounts
+	balanceBefore, err := wallet.Balance(context.Background(), utils.EthAddress, nil)
+	assert.NoError(t, err, "Balance should not return an error")
+
+	tokenBalanceBefore, err := token.BalanceOf(nil, wallet.Address())
+	assert.NoError(t, err, "BalanceOf should not return an error")
+
+	paymasterBalanceBefore, err := client.BalanceAt(context.Background(), paymasterAddress, nil)
+	assert.NoError(t, err, "BalanceAt should not return an error")
+
+	paymasterTokenBalanceBefore, err := token.BalanceOf(nil, paymasterAddress)
+	assert.NoError(t, err, "BalanceOf should not return an error")
+
+	calldata, err := tokenAbi.Pack("mint", wallet.Address(), MintAmount)
+	assert.NoError(t, err, "Pack should not return an error")
+
+	paymasterParams, err := utils.GetPaymasterParams(
+		paymasterAddress,
+		&types.ApprovalBasedPaymasterInput{
+			Token:            tokenAddress,
+			MinimalAllowance: MinimalAllowance,
+			InnerInput:       []byte{},
+		})
+	assert.NoError(t, err, "GetPaymasterParams should not return an error")
+
+	hash, err := wallet.SendTransaction(context.Background(), &accounts.Transaction{
+		To:   &tokenAddress,
+		Data: calldata,
+		Meta: &types.Eip712Meta{
+			PaymasterParams: paymasterParams,
+		},
+	})
+	assert.NoError(t, err, "SendTransaction should not return an error")
+
+	_, err = client.WaitMined(context.Background(), hash)
+	assert.NoError(t, err, "client.WaitMined should not return an error")
+
+	balanceAfter, err := wallet.Balance(context.Background(), utils.EthAddress, nil)
+	assert.NoError(t, err, "Balance should not return an error")
+
+	tokenBalanceAfter, err := token.BalanceOf(nil, wallet.Address())
+	assert.NoError(t, err, "BalanceOf should not return an error")
+
+	paymasterBalanceAfter, err := client.BalanceAt(context.Background(), paymasterAddress, nil)
+	assert.NoError(t, err, "BalanceAt should not return an error")
+
+	paymasterTokenBalanceAfter, err := token.BalanceOf(nil, paymasterAddress)
+	assert.NoError(t, err, "BalanceOf should not return an error")
+
+	assert.True(t, paymasterTokenBalanceBefore.Cmp(big.NewInt(0)) == 0, "Paymaster token balance before transaction should be 0")
+	assert.True(t, tokenBalanceBefore.Cmp(AirdropAmount) == 0, "Wallet token balance before transaction should be equal to airdrop amount")
+	assert.True(t, paymasterBalanceBefore.Cmp(paymasterBalanceAfter) > 0, "Paymaster balance after transaction should be decreased")
+	assert.True(t, paymasterTokenBalanceAfter.Cmp(MinimalAllowance) == 0, "Paymaster token balance after transaction should be minimal allowance amount")
+	assert.True(t, balanceBefore.Cmp(balanceAfter) == 0, "Balance should be the same")
+
+	tmp := new(big.Int).Add(tokenBalanceBefore, MintAmount)
+	assert.True(t, tokenBalanceAfter.Cmp(tmp.Sub(tmp, MinimalAllowance)) == 0, "Wallet token balance should be increased by difference between mint amount and allowance amount")
+}
--- test/setup_test.go
+++ test/setup_test.go
@@ -2,12 +2,10 @@ package test
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/zksync-sdk/zksync2-go/accounts"
 	"github.com/zksync-sdk/zksync2-go/clients"
@@ -18,132 +16,21 @@ import (
 	"time"
 )
 
-const TokenPath = "./token.json"
+const TokenPath = "./testfiles/tokens.json"
 
-type token struct {
-	L1Address string `json:"l1Address"`
-	L2Address string `json:"l2Address"`
-	L1TxHash  string `json:"L1TxHash"`
-	L2TxHash  string `json:"L2TxHash"`
-}
-
-func readToken() *token {
+func readTokens() []TokenData {
 	file, err := os.Open(TokenPath)
 	if err != nil {
-		log.Printf("Coud not find token.json, creating new one")
+		log.Printf("Could not find tokens.json")
 		return nil
 	}
-	defer func(file *os.File) {
-		errClose := file.Close()
-		if errClose != nil {
-			log.Fatalf("Error closing file: %s", err)
-		}
-	}(file)
 
-	var tokenData token
+	var tokens []TokenData
 	decoder := json.NewDecoder(file)
-	if errDecode := decoder.Decode(&tokenData); err != nil {
+	if errDecode := decoder.Decode(&tokens); err != nil {
 		log.Fatalf("Error decoding JSON: %s", errDecode)
 	}
-	return &tokenData
-}
-
-func writeToken(token token) {
-	file, err := os.Create(TokenPath)
-	if err != nil {
-		log.Fatalf("Error creating file: %s", err)
-	}
-	defer func(file *os.File) {
-		errClose := file.Close()
-		if errClose != nil {
-			log.Fatalf("Error closing file: %s", err)
-		}
-	}(file)
-
-	// Marshal data to JSON
-	jsonData, err := json.MarshalIndent(token, "", "  ")
-	if err != nil {
-		log.Fatalf("Error marshaling to JSON: %s", err)
-	}
-
-	// Write JSON data to the file
-	_, err = file.Write(jsonData)
-	if err != nil {
-		log.Fatalf("Error writing JSON to file: %s", err)
-	}
-}
-
-func checkIfTokenNeedsToBeCreated(client clients.Client, ethClient *ethclient.Client) (bool, bool) {
-	tokenData := readToken()
-	if tokenData == nil {
-		return true, true
-	}
-	_, err := ethClient.CodeAt(context.Background(), common.HexToAddress(tokenData.L1Address), nil)
-	if err != nil {
-		return false, false
-	}
-	_, err = client.CodeAt(context.Background(), common.HexToAddress(tokenData.L2Address), nil)
-	if err != nil {
-		return true, false
-	}
-
-	return true, true
-}
-
-func createTokenL1(ethClient *ethclient.Client, privateKey *ecdsa.PrivateKey, publicKey common.Address) common.Address {
-	chainID, err := ethClient.ChainID(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
-	auth, err := bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	address, tx, tokenContract, err := DeployToken(auth, ethClient, "DAI", "DAI", 18)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = bind.WaitDeployed(context.Background(), ethClient, tx)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	symbol, err := tokenContract.Symbol(nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	decimals, err := tokenContract.Decimals(nil)
-	if err != nil {
-		log.Fatal(nil)
-	}
-
-	auth, err = bind.NewKeyedTransactorWithChainID(privateKey, chainID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	tx, err = tokenContract.Mint(auth, publicKey, big.NewInt(100_000_000))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_, err = bind.WaitMined(context.Background(), ethClient, tx)
-	if err != nil {
-		log.Fatal("Wait mint: ", err)
-	}
-
-	balance, err := tokenContract.BalanceOf(nil, publicKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	log.Println("Token address: ", address)
-	log.Println("Symbol:", symbol)
-	log.Println("Decimals:", decimals)
-	log.Println("Balance L1: ", balance)
-
-	return address
+	return tokens
 }
 
 func createTokenL2(wallet *accounts.Wallet, client clients.Client, ethClient *ethclient.Client, l1Token common.Address) (common.Address, common.Hash, common.Hash) {
@@ -179,12 +66,12 @@ func createTokenL2(wallet *accounts.Wallet, client clients.Client, ethClient *et
 
 	tokenL2Address, err := client.L2TokenAddress(context.Background(), l1Token)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	tokenL2Balance, err := wallet.Balance(context.Background(), tokenL2Address, nil)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Println("Balance L2: ", tokenL2Balance)
@@ -221,7 +108,7 @@ func TestMain(m *testing.M) {
 
 	client, err := clients.Dial(ZkSyncEraProvider)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 	defer client.Close()
 
@@ -230,44 +117,15 @@ func TestMain(m *testing.M) {
 		log.Fatal(err)
 	}
 
-	privateKey, err := crypto.HexToECDSA(PrivateKey)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	publicKeyECDSA, ok := privateKey.Public().(*ecdsa.PublicKey)
-	if !ok {
-		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
-	}
-	publicKey := crypto.PubkeyToAddress(*publicKeyECDSA)
-
 	wallet, err := accounts.NewWallet(common.Hex2Bytes(PrivateKey), &client, ethClient)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	shouldCreateL1Token, shouldCreateL2Token := checkIfTokenNeedsToBeCreated(client, ethClient)
-	if shouldCreateL1Token {
-		l1TokenAddress := createTokenL1(ethClient, privateKey, publicKey)
-		l2TokenAddress, l1Tx, l2Tx := createTokenL2(wallet, client, ethClient, l1TokenAddress)
-		writeToken(token{
-			L1Address: l1TokenAddress.Hex(),
-			L2Address: l2TokenAddress.Hex(),
-			L1TxHash:  l1Tx.Hex(),
-			L2TxHash:  l2Tx.Hex(),
-		})
+	L1Tokens = readTokens()
+	L1Dai = L1Tokens[0].Address
 
-	} else if !shouldCreateL1Token && shouldCreateL2Token {
-		tokenData := readToken()
-		l2TokenAddress, l1Tx, l2Tx := createTokenL2(wallet, client, ethClient, common.HexToAddress(tokenData.L1Address))
-		tokenData.L2Address = l2TokenAddress.Hex()
-		tokenData.L1TxHash = l1Tx.Hex()
-		tokenData.L1TxHash = l2Tx.Hex()
-
-		writeToken(*tokenData)
-	} else {
-		fmt.Println("Token has been already created.")
-	}
+	L2Dai, L1DepositTx, L2DepositTx = createTokenL2(wallet, client, ethClient, L1Dai)
 
 	os.Exit(m.Run())
 }
