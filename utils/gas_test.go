54d1f1f2c56e86e6f9dc8349fc8f59d28f6a3dd5
danijelTxFusion <danijel@txfusion.io>
2024-02-22 20:34:30 +0100

    test(utils): add unit tests

---
+++ utils/gas_test.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math/big"
+	"testing"
+)
+
+func TestCheckBaseCost(t *testing.T) {
+	err := CheckBaseCost(big.NewInt(100), big.NewInt(99))
+	assert.Error(t, err, "Should throw error when base cost is greater than value")
+	assert.ErrorContains(t, err, "the base cost of performing the priority operation is higher")
+}
