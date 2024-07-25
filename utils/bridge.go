fcdea3bacc4094c91034afdcd3b6cd87f5cdaebd
danijelTxFusion <danijel@txfusion.io>
2024-07-26 00:40:02 +0200

    feat(utils): add `HashedL2ToL1Msg` method

--- utils/bridge.go
+++ utils/bridge.go
@@ -1,10 +1,12 @@
 package utils
 
 import (
+	"encoding/binary"
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/zksync-sdk/zksync2-go/contracts/erc20"
 	"github.com/zksync-sdk/zksync2-go/contracts/l2bridge"
 	"github.com/zksync-sdk/zksync2-go/types"
@@ -90,4 +92,18 @@ func Erc20BridgeCalldata(l1TokenAddress, l1Sender, l2Receiver common.Address, am
 		return nil, fmt.Errorf("failed to load L2 bridge ABI: %w", err)
 	}
 	return l2BridgeAbi.Pack("finalizeDeposit", l1Sender, l2Receiver, l1TokenAddress, amount, bridgeData)
+}
+
+// HashedL2ToL1Msg returns a `keccak` encoded message with a given sender address and
+// block number from the L1 messenger contract.
+func HashedL2ToL1Msg(sender common.Address, msg []byte, txNumberInBlock uint16) common.Hash {
+	txNumberInBlockBytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(txNumberInBlockBytes, txNumberInBlock)
+
+	encodedMsg := append([]byte{0, 1}, txNumberInBlockBytes...)
+	encodedMsg = append(encodedMsg, L1MessengerAddress.Bytes()...)
+	encodedMsg = append(encodedMsg, common.LeftPadBytes(sender.Bytes(), 32)...)
+	encodedMsg = append(encodedMsg, crypto.Keccak256(msg)...)
+
+	return crypto.Keccak256Hash(encodedMsg)
 }
