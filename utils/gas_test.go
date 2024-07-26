6976fbf916eb251d1c4450896d2c880de35ea42f
danijelTxFusion <danijel@txfusion.io>
2024-07-27 00:11:40 +0200

    feat(utils): add `IsTypedDataSignatureCorrect` function

--- utils/gas_test.go
+++ utils/gas_test.go
@@ -8,6 +8,6 @@ import (
 
 func TestCheckBaseCost(t *testing.T) {
 	err := CheckBaseCost(big.NewInt(100), big.NewInt(99))
-	assert.Error(t, err, "Should throw error when base cost is greater than value")
-	assert.ErrorContains(t, err, "the base cost of performing the priority operation is higher")
+	assert.Error(t, err, "CheckBaseCost Should return error when base cost is greater than value")
+	assert.ErrorContains(t, err, "The base cost of performing the priority operation is higher")
 }
