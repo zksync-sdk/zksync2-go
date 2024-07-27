d45412c378a52fb14c59323f93a546c78a5b57a0
danijelTxFusion <danijel@txfusion.io>
2024-07-27 13:46:12 +0200

    test(utils): add test from `ScaleGasLimit` function

--- utils/gas_test.go
+++ utils/gas_test.go
@@ -11,3 +11,8 @@ func TestCheckBaseCost(t *testing.T) {
 	assert.Error(t, err, "CheckBaseCost Should return error when base cost is greater than value")
 	assert.ErrorContains(t, err, "The base cost of performing the priority operation is higher")
 }
+
+func TestScaleGasLimit(t *testing.T) {
+	actual := ScaleGasLimit(big.NewInt(100))
+	assert.True(t, big.NewInt(120).Cmp(actual) == 0, "Amounts should be the same")
+}
